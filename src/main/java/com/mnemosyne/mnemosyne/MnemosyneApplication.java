bd07bebdd2082b3080a7f02c7b22d858e35d1dca
洛先生 <316328473@qq.com>
2018-04-23 17:43:36 +0800

    fix

---
+++ src/main/java/com/mnemosyne/mnemosyne/MnemosyneApplication.java
@@ -0,0 +1,12 @@
+package com.mnemosyne.mnemosyne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnemosyneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnemosyneApplication.class, args);
+	}
+}
